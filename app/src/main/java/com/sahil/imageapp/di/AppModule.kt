106d1690fc916bf448fd47b182cc737d7c9e8cc9
sahil1918912 <sahilchauhank6428@gmail.com>
2024-12-03 20:11:44 +0530

    Add fynctionality of Image Download

--- app/src/main/java/com/sahil/imageapp/di/AppModule.kt
+++ app/src/main/java/com/sahil/imageapp/di/AppModule.kt
@@ -1,13 +1,17 @@
 package com.sahil.imageapp.di
 
+import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sahil.imageapp.data.remote.UnSplashApiService
+import com.sahil.imageapp.data.repository.AndroidImageDownloader
 import com.sahil.imageapp.data.repository.ImageRepositoryImpl
 import com.sahil.imageapp.data.util.Constants
+import com.sahil.imageapp.domain.repository.Downloader
 import com.sahil.imageapp.domain.repository.ImageRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -37,5 +41,13 @@ object AppModule {
     @Singleton
     fun provideImageRepository(apiService: UnSplashApiService) : ImageRepository {
         return ImageRepositoryImpl(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAndroidImageDownloader(
+        @ApplicationContext context: Context
+    ) : Downloader {
+        return AndroidImageDownloader(context)
     }
 }
